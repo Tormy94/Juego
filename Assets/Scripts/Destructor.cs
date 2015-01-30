6560ecd9d741385b71e3429fb11af0a1b72d5091
Tormy94 <victor.manuel.lopez.pardo@gmail.com>
2015-01-30 16:44:17 +0100

    Cambio en Leonardo
    Ahora leonardo se crea a partir de un generador cada vez que desaparece.
    Hay que controlar un fallo que sale si muere varias veces seguidas y el
    problema de cargar la escena desde el menu principal

--- Assets/Scripts/Destructor.cs
+++ Assets/Scripts/Destructor.cs
@@ -15,18 +15,22 @@ public class Destructor : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other){
 		string tag = other.tag;
+		GameObject Leonardo = GameObject.Find("Leonardo(Clone)");
 		switch (tag) {
 		case "Player":
 			NotificationCenter.DefaultCenter().PostNotification(this,"PersonajeHaMuerto");
 			GameObject oscar = GameObject.Find("Player");
+
 			//GameObject leo = GameObject.Find("Leonardo");
 			//leo.SetActive(false);
 			oscar.SetActive(false);
+			Destroy(Leonardo);
 			break;
 		case "Leonardo":
 			//Quitar comentarios cuando hagamos la implementacion del contador de Leonardo
 		//	NotificationCenter.DefaultCenter().PostNotification(this, "LeonardoHaMuerto");
 			//Destroy (other.gameObject);
+			Destroy(Leonardo);
 			break;
 		default: 
 			Destroy (other.gameObject);
---
+++ Assets/Scripts/GeneradorLeonardo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneradorLeonardo : MonoBehaviour {
+
+	public GameObject Leonardo;
+	//GameObject oscar = GameObject.Find ("Player");
+	private bool fin = false;
+
+	// Use this for initialization
+	void Start () {
+
+		NotificationCenter.DefaultCenter().AddObserver(this, "PersonajeHaMuerto");
+		NotificationCenter.DefaultCenter().AddObserver(this, "PersonajeEmpiezaACorrer");
+	}
+
+	void PersonajeHaMuerto(){
+		fin = true;
+	}
+	void PersonajeEmpiezaACorrer(Notification noficacion){
+		Generar ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.Find("Leonardo(Clone)")== null) {
+			Generar ();
+		}
+	}
+
+	void Generar(){
+		if (!fin) {
+			Instantiate (Leonardo, transform.position, Quaternion.identity);
+		}
+	}
+}
